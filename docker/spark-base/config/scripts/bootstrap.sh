5ba04ad1d116e7fee7d9c3e061f31ad8530bc418
Gabriel B <gabriel.benvegmi@outlook.com>
2020-08-25 14:16:59 -0300

    Primeira versao distribuida

--- docker/spark-base/config/scripts/bootstrap.sh
+++ docker/spark-base/config/scripts/bootstrap.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-/etc/init.d/ssh start
 hdfs namenode -format
+/etc/init.d/ssh start
 if [[ $HOSTNAME = spark-master ]]; then
     
     $HADOOP_HOME/sbin/start-dfs.sh
     $HADOOP_HOME/sbin/start-yarn.sh
     #start-master.sh
+
+else
+    # useradd hadoop
+    /usr/hadoop-2.7.3/bin/hadoop-daemon.sh start datanode
+    /usr/hadoop-2.7.3/bin/yarn nodemanager
 
 fi
 #bash
